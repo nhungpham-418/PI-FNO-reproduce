cc4c4b2ecd05a72fbf9dbb5456940d3f7e5c789c
Nhung Pham <tp526@a1128u44n01.mghpcc.ycrc.yale.edu>
2026-04-15 07:57:38 -0400

    new plot

---
+++ scripts/burgers2d-32-mode-8-data-1-pde-0.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=burgers2d-32-mode-8-data-1-pde-0
+#SBATCH --output=logs/burgers2d-32-mode-8-data-1-pde-0.txt
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=32G
+#SBATCH --partition=gpu_h200
+#SBATCH --gpus=h200:1
+#SBATCH --time=0:30:00
+
+module load miniconda
+conda activate dde
+
+cd /home/tp526/neuraloperator/PI-FNO-reproduce
+
+python3 train_burgers2d.py --config_path configs/pretrain/burgers2d_inviscid_pretrain_data_small.yaml
---
+++ scripts/burgers2d-32-mode-8-ic-100-pde-1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=burgers2d-32-mode-8-ic-100-pde-1
+#SBATCH --output=logs/burgers2d-32-mode-8-ic-100-pde-1.txt
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=32G
+#SBATCH --partition=gpu_h200
+#SBATCH --gpus=h200:1
+#SBATCH --time=0:45:00
+
+module load miniconda
+conda activate dde
+
+cd /home/tp526/neuraloperator/PI-FNO-reproduce
+
+python3 train_burgers2d.py --config_path configs/pretrain/burgers2d_inviscid_pretrain_physics_2.yaml
